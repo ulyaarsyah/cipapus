824487c0eb1a71d8536d9eb0d912068ee8687cab
ulyaarsyah <ulya@pnp.ac.id>
2023-03-16 02:03:31 -0700

    first commit

---
+++ src/main/java/com/mycompany/cipapus/Cipapus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cipapus;
+
+/**
+ *
+ * @author Ulya
+ */
+public class Cipapus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
